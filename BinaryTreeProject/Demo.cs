7fa1a4038c6695bf9e35530a33001ed20c07bb5e
MazaharAhamed <mazaharahamed77@gmail.com>
2020-08-18 14:08:55 +0530

    Add project files.

---
+++ BinaryTreeProject/Demo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTreeProject
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree bt = new BinaryTree();
+
+            bt.createBinaryTree();
+
+            //Console.WriteLine("Preorder of binary tree is: "bt.Preorder());
+
+            bt.Preorder();
+            bt.Inorder();
+            bt.Postorder();
+            bt.Height();
+            bt.Display();
+        }
+    }
+}
---
+++ BinaryTreeProject/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTreeProject
+{
+    class Node
+    {
+        public Node lchild;
+        public Node rchild;
+        public char info;
+
+        public Node(char ch)
+        {
+            info = ch;
+            lchild = null;
+            rchild = null;
+        }
+    }
+}
